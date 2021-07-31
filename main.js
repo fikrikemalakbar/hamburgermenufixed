a024849b2fc5342cf10204c1bd600eaf19a3f8ac
fikrikemalakbar <fikrisangmaaestromidfielder@gmail.com>
2021-07-31 07:07:41 +0700

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mainoffcanvas").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mainoffcanvas").style.marginLeft= "0";
+}
+
